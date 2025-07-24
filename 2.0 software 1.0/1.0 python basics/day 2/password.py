83ae9436c373e582d02bd091441bd892155a5540
MP <empressbuchu74@gmail.com>
2025-07-24 17:07:14 +0100

    Password validation:  checking whether it has at least one uppercase, lowercase and digit

--- 2.0 software 1.0/1.0 python basics/day 2/password.py
+++ 2.0 software 1.0/1.0 python basics/day 2/password.py
@@ -1,9 +1,23 @@
-password=input("Enter your password")
 
-lower_case=password.islower()
-upper_case=password.isupper()
-numeric=password.isdigit()
-if lower_case and upper_case and numeric:
-    print("This is a strong password.")
+
+l=False
+u=False
+d=False
+
+password=input("A strong password must contain at least one uppercase, one lowercase and one digit. \nEnter Strong Password: ")
+
+for letter in password:
+    if letter.islower():
+        l=True
+    elif letter.isupper():
+        u=True
+    elif letter.isdigit():
+        d=True
+if l and u and d:
+    print("This is a strong password")
 else:
-    print("Please type a strong password")
+    print("Please type in a strong password")
+    
+    
+    # Assignment check for the length atleast 8 chars
+    # Equally make it to loop till one types the correct password then stops
