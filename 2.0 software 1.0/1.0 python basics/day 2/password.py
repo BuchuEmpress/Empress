4e7cb1f4b80cab146eaffffbaa07898f9d45629c
MP <empressbuchu74@gmail.com>
2025-07-25 07:35:20 +0100

    made few changes such that the length of password would be checked and loops until a strong password is typed

--- 2.0 software 1.0/1.0 python basics/day 2/password.py
+++ 2.0 software 1.0/1.0 python basics/day 2/password.py
@@ -4,20 +4,27 @@ l=False
 u=False
 d=False
 
-password=input("A strong password must contain at least one uppercase, one lowercase and one digit. \nEnter Strong Password: ")
 
-for letter in password:
+while True:
+ password=input("A strong password must contain at least one uppercase, one lowercase and one digit. \nEnter Strong Password: ")
+
+ if len(password) < 8:
+        print ("Passwords must cotain at least 8 characters")
+        continue
+ for letter in password:
     if letter.islower():
         l=True
-    elif letter.isupper():
+    if letter.isupper():                    # can continue with elif without while loop
         u=True
-    elif letter.isdigit():
+    if letter.isdigit():
         d=True
-if l and u and d:
-    print("This is a strong password")
-else:
-    print("Please type in a strong password")
-    
+ if l and u and d:
+     print("This is a strong password")
+     break
+
+ else:
+     print("Please type in a strong password")
+     
     
     # Assignment check for the length atleast 8 chars
     # Equally make it to loop till one types the correct password then stops
