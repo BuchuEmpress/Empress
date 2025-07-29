7b6f7b07287305ff7bd653441371a2dd86d4f828
MP <empressbuchu74@gmail.com>
2025-07-29 15:59:18 +0100

    Learnt some list commands to add to list, remove frome list, pop from list to store in a value, change an item

--- 2.0 software 1.0/2.0 python intermediate/day 3/data structures/lists.py
+++ 2.0 software 1.0/2.0 python intermediate/day 3/data structures/lists.py
@@ -14,5 +14,43 @@ print(f"The second fruit is: {fruits[1]}")
 print(f"The third fruit is: {fruits[2]}")
 print(f"The fourth fruit is: {fruits[3]}")
 
+
 print(f"First three fruits are: {fruits[0:3]}")
 print(f"First fruit till end: {fruits[0:5]}")
+print(f"Every second fruit: {fruits[::2]}")
+print(f"Second fruit till the end: {fruits[-2]}")
+print(f"Last fruit: {fruits[-1]}")
+
+# modifying the list
+groceries=["milk", "bread", "eggs"]
+print(f"Original List is: {groceries}")
+
+# adding a single item to the list
+groceries.append("cheese")
+print(f"After adding a single item, we have: {groceries}")
+
+# adding multiple items to the list
+groceries.extend(["apples", "bananas"])
+print(f"After adding more than one item, we have: {groceries}")
+
+# adding an item at a specific index
+groceries.insert(1, "yogurt")
+print(f"After adding \"Yogurt\" at point two, we have: {groceries}")
+
+# changing an existing item
+groceries[0]="Almond milk"
+print(f"After changing \"Milk\" to \"Almond Milk\" we have: {groceries}")
+
+
+# removing an item
+groceries.remove("bread")
+print(f"The list after removing bread: {groceries}")
+
+removed_item= groceries.pop()       #remove and return the last item
+print(f"Removed item: {removed_item}")  # this creates a holder for the removed item and displays it
+print(f"After popping: {groceries}")
+
+specific_item= groceries.pop(1)     #remove and return item at index one
+print(f"Removed item at index one: {specific_item}")
+print(f"Final List: {groceries}")
+
